07fe8743cde4d5c6a756249c6cff6bb9fcacd62a
Pieter Steyaert <pietersteyaert@Pieters-MacBook-Pro.local>
2018-11-09 14:43:40 +0100

    index

--- server.js
+++ server.js
@@ -2,9 +2,23 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require("path");
+const csv = require("csvtojson");
+var jsondata = undefined;
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+let init =()=>{
+    csv()
+        .fromFile("./data/meteors.csv")
+        .then((parsed) => {
+            jsondata = parsed;
+            
+            app.listen(port, () => console.log(`Meteorite app listening on port ${port}!`));
+        }, (error)=>{
+            console.log(error);
+        })
+};
+
+init();
