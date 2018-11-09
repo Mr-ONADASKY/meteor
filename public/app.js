2bb15800f444556eb653f2e8ac7d5c98e892668c
Pieter Steyaert <pietersteyaert@Pieters-MacBook-Pro.local>
2018-11-09 15:18:25 +0100

    index

--- public/app.js
+++ public/app.js
@@ -1,1 +1,22 @@
-alert("hello");
+var nasa = {};
+
+var datarequest = new XMLHttpRequest();
+datarequest.open('GET', '/data', true);
+datarequest.onload = function () {
+    //load data
+    var data = JSON.parse(this.response);
+    console.log(data);
+    
+    // let str = "<ul>";
+    // data.nasa.forEach(element => {
+    //     str += "<li>" + element.name + " -- " + element.mass + "</li>";
+    // });
+    // str += "</ul>";
+    //populate raw data
+    // document.getElementById(settings.rawID).innerHTML = str;
+    //assign globals
+    nasa.data = data.nasa;
+}
+
+// Send request
+datarequest.send();
