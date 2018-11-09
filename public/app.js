12e5dfbcd569132b11db71b70622b409f37599d4
Pieter Steyaert <pietersteyaert@Pieters-MacBook-Pro.local>
2018-11-09 15:38:58 +0100

    index

--- public/app.js
+++ public/app.js
@@ -1,22 +1,31 @@
 var nasa = {};
 
-var datarequest = new XMLHttpRequest();
-datarequest.open('GET', '/data', true);
-datarequest.onload = function () {
-    //load data
-    var data = JSON.parse(this.response);
-    console.log(data);
+var settingrequest = new XMLHttpRequest();
+settingrequest.open('GET', '/settings', true);
+settingrequest.onload = function () {
+    var settings = JSON.parse(this.response);
+    console.log(settings);
     
-    let str = "<ul>";
-    data.forEach(element => {
-        str += "<li>" + element.name + " -- " + element.mass + "</li>";
-    });
-    str += "</ul>";
-    //populate raw data
-    document.getElementById("data").innerHTML = str;
-    //assign globals
-    nasa.data = data;
-}
+    var datarequest = new XMLHttpRequest();
+    datarequest.open('GET', '/data', true);
+    datarequest.onload = function () {
+        //load data
+        var data = JSON.parse(this.response);
+        console.log(data);
 
-// Send request
-datarequest.send();
+        let str = "<ul>";
+        data.forEach(element => {
+            str += "<li>" + element.name + " -- " + element.mass + "</li>";
+        });
+        str += "</ul>";
+        //populate raw data
+        document.getElementById("data").innerHTML = str;
+        //assign globals
+        nasa.data = data;
+    }
+
+    // Send request
+    datarequest.send();
+
+}
+settingrequest.send();
